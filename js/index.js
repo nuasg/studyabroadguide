5451e0db377a5b842f698f425214bbb77c38272d
divir94 <divir94@gmail.com>
2014-05-05 02:34:31 -0500

    -

--- js/index.js
+++ js/index.js
@@ -9,11 +9,25 @@ $( document ).ready(function() {
 	    $('.' + $id).show("slide", {
 	        direction: "right"
 	    }, 300);
-
-	    //$(".program-list").css({'background-color': 'transparent'}); 
-	    //$('#'+ $id).css({'background-color': '#aaa'}); 
-	    
 	    $('li').removeClass('selected');
     	$(this).addClass('selected');
 	});
+
+
+	$('.eat-place-1').show();
+
+/*
+	$('.eat-list').click(function() {
+
+		var $id = this.id;
+		alert($id);
+		$('.eat-info').hide();
+	    $('.' + $id).show("slide", {
+	        direction: "right"
+	    }, 300);
+	    $('li').removeClass('selected');
+    	$(this).addClass('selected');
+	});
+
+*/
 });
