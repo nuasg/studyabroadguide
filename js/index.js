51ff9ff172181bf7f1e6ab14d3ca93c45ed4a5e9
divir94 <divir94@gmail.com>
2014-05-05 01:54:00 -0500

    added animations to programs

--- js/index.js
+++ js/index.js
@@ -1,10 +1,19 @@
 $( document ).ready(function() {
-	$('.btn').click(function() {
+	$('.program-4').show();
+
+	$('.program-list').click(function() {
+
 		var $id = this.id;
-		$('.box').hide();
+		//alert($id);
+		$('.program-info').hide();
 	    $('.' + $id).show("slide", {
 	        direction: "right"
-	    }, 500);
+	    }, 300);
 
+	    //$(".program-list").css({'background-color': 'transparent'}); 
+	    //$('#'+ $id).css({'background-color': '#aaa'}); 
+	    
+	    $('li').removeClass('selected');
+    	$(this).addClass('selected');
 	});
 });
